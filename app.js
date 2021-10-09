db0b80be4c31ace338718f3b7c8f4bed29809208
Thinh Pham <phamducthien19012006@gmail.com>
2021-10-09 22:02:31 +0700

    lesson5

---
+++ app.js
@@ -0,0 +1,89 @@
+// let animals = {
+//   dog: {
+//     age: 2,
+//     hobby: sleep,
+//     name: xuka,
+//   },
+// };
+// let data = {
+//   page: 2,
+//   per_page: 6,
+//   total: 12,
+//   total_pages: 2,
+//   data: [
+//     {
+//       id: 7,
+//       email: "michael.lawson@reqres.in",
+//       first_name: "Michael",
+//       last_name: "Lawson",
+//       avatar: "https://reqres.in/img/faces/7-image.jpg",
+//     },
+//     {
+//       id: 8,
+//       email: "lindsay.ferguson@reqres.in",
+//       first_name: "Lindsay",
+//       last_name: "Ferguson",
+//       avatar: "https://reqres.in/img/faces/8-image.jpg",
+//     },
+//     {
+//       id: 9,
+//       email: "tobias.funke@reqres.in",
+//       first_name: "Tobias",
+//       last_name: "Funke",
+//       avatar: "https://reqres.in/img/faces/9-image.jpg",
+//     },
+//     {
+//       id: 10,
+//       email: "byron.fields@reqres.in",
+//       first_name: "Byron",
+//       last_name: "Fields",
+//       avatar: "https://reqres.in/img/faces/10-image.jpg",
+//     },
+//     {
+//       id: 11,
+//       email: "george.edwards@reqres.in",
+//       first_name: "George",
+//       last_name: "Edwards",
+//       avatar: "https://reqres.in/img/faces/11-image.jpg",
+//     },
+//     {
+//       id: 12,
+//       email: "rachel.howell@reqres.in",
+//       first_name: "Rachel",
+//       last_name: "Howell",
+//       avatar: "https://reqres.in/img/faces/12-image.jpg",
+//     },
+//   ],
+//   support: {
+//     url: "https://reqres.in/#support-heading",
+//     text: "To keep ReqRes free, contributions towards server costs are appreciated!",
+//   },
+// };
+// let users = data["data"];
+// let total = data["total"];
+// let text = data["support"]["text"];
+// console.log(users);
+// console.log(total);
+// console.log(text);
+// let emails = [];
+// for (let i = 0; i < data.data.length; i++) {
+//   let temp = data.data[i];
+//   let email = temp.email;
+//   emails.push(email);
+// }
+// console.log(emails);
+let pokemon = {
+  name: "Pikachu",
+  stats: {
+    hp: 100,
+    level: 50,
+    attack: 86,
+    spattack: 150,
+    speed: 5000000000,
+    belongTo: "Red",
+  },
+};
+let pikachuSkill = ["Bite", "Thunder Bolt", "Volt Tackle", "Thunderrrr!"];
+pokemon.stats.speed = 60;
+pokemon.skill = pikachuSkill;
+delete pokemon.belongTo;
---
+++ homework/lesson-05/app.js
@@ -0,0 +1,75 @@
+inventory = {
+  gold: 500,
+  pouch: ["flint", "twint", "gemstone"],
+  backpack: ["xylophone", "dagger", "bedroll", "bread loaf"],
+};
+inventory.pocket = ["seashell", "strange berry", "lint"];
+inventory.backpack.splice(1, 1);
+inventory.gold += 50;
+delete inventory.pouch;
+console.log(inventory);
+
+function getBudgets(data) {
+  let box = [];
+  let a = 0;
+  for (let i = 0; i < data.length; i++) {
+    let temp = data[i];
+    let budget = temp.budget;
+    budget = a + budget;
+    a = budget;
+  }
+  return a;
+}
+console.log(
+  getBudgets([
+    { name: "John", age: 21, budget: 29000 },
+    { name: "Steve", age: 32, budget: 32000 },
+    { name: "Martin", age: 16, budget: 1600 },
+  ])
+);
+const GUEST_LIST = {
+  Randy: "Germany",
+  Karla: "France",
+  Wendy: "Japan",
+  Norman: "England",
+  Sam: "Argentina",
+};
+function greeting(a) {
+  if (a == "Randy") {
+    console.log(`\"Hi! I\'m ${a}, and I\'m from ${GUEST_LIST.Randy}.\"`);
+  } else if (a == "Karla") {
+    console.log(`\"Hi! I\'m ${a}, and I\'m from ${GUEST_LIST.Karla}.\"`);
+  } else if (a == "Wendy") {
+    console.log(`\"Hi! I\'m ${a}, and I\'m from ${GUEST_LIST.Wendy}.\"`);
+  } else if (a == "Norman") {
+    console.log(`\"Hi! I\'m ${a}, and I\'m from ${GUEST_LIST.Norman}.\"`);
+  } else if (a == "Sam") {
+    console.log(`\"Hi! I\'m ${a}, and I\'m from ${GUEST_LIST.Sam}.\"`);
+  } else {
+    console.log('"Hi! I\'m a guest."');
+  }
+}
+greeting("Sam");
+function keysAndValues(a) {
+  let box = [];
+  let key = Object.keys(a);
+  let value = Object.values(a);
+  box.push(key, value);
+  console.log(box);
+}
+keysAndValues({ a: 1, b: 2, c: 3 });
+function inkLevels(a) {
+  let value = Object.values(a);
+  let temp = value[0];
+  for (let i = 0; i < value.length; i++) {
+    if (temp >= value[i]) {
+      temp = value[i];
+    }
+  }
+  console.log(temp);
+}
+inkLevels({
+  cyan: 888,
+  magenta: 543,
+  yellow: 777,
+});
